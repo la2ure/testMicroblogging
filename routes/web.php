32e9b072b631d57bdeb63365db88d2454ad98782
b-khadija <khadija.bayssir@gmail.com>
2023-08-08 15:23:35 +0200

    Delete all files for Posts

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,22 +18,16 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [HomeController::class, 'homepage']);
 
-Route::resource('posts', PostController::class);
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
-// Route::get('post', [HomeController::class, 'post'])->middleware(['auth', 'admin']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::resource("posts", PostController::class);
 
 require __DIR__.'/auth.php';
