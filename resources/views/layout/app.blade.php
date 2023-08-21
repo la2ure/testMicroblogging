802b1a705bbe5fdb4dbf1879cc34203b84682bc2
b-khadija <khadija.bayssir@gmail.com>
2023-08-21 11:42:15 +0200

    Resize grid, display post & change style of buttons

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,12 +14,12 @@
   <div class="container mx-auto">
     <div class="row my-2">
       <div class="col-lg-12 d-flex justify-content-between align-items-center mx-auto">
-        <div>
-          <h2 class="text-primary">@yield('heading')</h2>
-        </div>
-        <div>
-          <a href="@yield('link')" class="btn btn-primary rounded-pill">@yield('link_text')</a>
-        </div>
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+          <h2 class="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white bg-transparent rounded-md group-hover:bg-opacity-0">@yield('heading')</h2>
+        </button>
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+          <a href="@yield('link')" class="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white bg-transparent rounded-md group-hover:bg-opacity-0">@yield('link_text')</a>
+        </button>
 
       </div>
     </div>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.app')
+
 @section('title', 'Home Page')
 @section('heading', 'All Posts')
 @section('link_text', 'Add New Post')
@@ -12,42 +13,25 @@
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
-<div class="grid grid-cols-1 sm:grid-cols-3 gap-4 min-h-screen">
+
+<div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
   @forelse($posts as $key => $row)
-<div class="relative flex flex-col items-center justify-center">
-  <div class="relative h-56 w-full mb-3">
-      <div >
+    <div class="relative flex flex-col mb-9">
+      <div class="relative h-80 w-full">
         <a href="post/{{ $row->id }}">
           <img src="{{ asset('storage/images/'.$row->image) }}" alt="" class="block object-cover h-full w-full"/>
         </a>
-        <div class="flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-      </div>
-      <div>
-        <p class="btn btn-success rounded-pill btn-sm">{{ $row->tag }}</p>
-      </div>
-  </div>
-</div>
-
-
-  {{-- <div class="col-lg-4">
-      <div class="card shadow">
-        <a href="post/{{ $row->id }}">
-          <img src="{{ asset('storage/images/'.$row->image) }}" class="card-img-top img-fluid">
-        </a>
-        <div class="card-body">
+        <div>
           <p class="btn btn-success rounded-pill btn-sm">{{ $row->tag }}</p>
-          {{-- <div class="card-title fw-bold text-primary h4">{{ $row->title }}</div> --}}
-          {{-- <p class="text-secondary">{{ Str::limit($row->content, 100) }}</p> --}}
-        {{-- </div>
+        </div>
       </div>
-  </div> --}} 
-
+    </div>
   @empty
     <h2 class="text-center text-secondary p-4">No post found in the database!🧧🏁</h2>
   @endforelse
   <div class="d-flex justify-content-center my-5">
     {{ $posts->onEachSide(1)->links() }}
   </div>
-
 </div>
-@endsection 
+
+@endsection
